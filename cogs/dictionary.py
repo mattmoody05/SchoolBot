d79ee856c16e525af307f232eba69cf632e5fccb
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2020-10-10 11:33:33 +0100

    finished dictionary cog

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -1,49 +1,49 @@
+# discord imports
 import discord
 from discord.ext import commands
 
+# dictionary imports
 from PyDictionary import PyDictionary
 dictionary = PyDictionary()
-import requests
 
-class define(commands.Cog):
+
+class dictionary(commands.Cog):
     def __init__(self, client):
         self.client = client
+
 
     @commands.command()
     async def define(self, ctx, arg):
         Definition = dictionary.meaning(arg)
-        NumberOfDefinitions = len(Definition)
+        
         DefinitionEmbed = discord.Embed(
             colour = discord.Colour.light_gray()
         )
         DefinitionEmbed.set_author(name = 'Definition of "{}"'.format(arg))
-        for i in range(NumberOfDefinitions):
-            DefinitionEmbed.add_field(name = list(Definition)[i], value=(Definition[list(Definition)[i]]), inline=False)
+        
+        for i in range(len(Definition)):
+            CurrentDef = str(Definition[list(Definition)[i]]).replace("[", "").replace("]", "")
+            DefinitionEmbed.add_field(name = list(Definition)[i], value=CurrentDef, inline=False)
         
         await ctx.send(embed = DefinitionEmbed)
 
     
     @commands.command()
     async def synonym(self, ctx, arg):
-        Synonyms = str(dictionary.synonym(arg)).replace("[", "").replace("]", "").split(",")
-        await ctx.send(Synonyms)
-
+        Synonyms = str(dictionary.synonym(arg)).replace("[", "").replace("]", "").replace("'", "").split(",")
+        
+        string = ""
+        for i in range(len(Synonyms)):
+            string = "{0}\n{1}".format(string, Synonyms[i])
+        
         SynonymEmbed = discord.Embed(
-            colour = discord.Colour.light_gray()
+            colour = discord.Colour.light_gray(),
+            description = string
         )
         SynonymEmbed.set_author(name='Synonyms for "{}"'.format(arg))
-        for i in range(len(Synonyms)):
-            SynonymEmbed.add_field(name=Synonyms[i], value=dictionary.meaning(Synonyms[i]))
-            print(dictionary.meaning(Synonyms[i]))
-        
 
         await ctx.send(embed = SynonymEmbed)
 
-    @commands.command()
-    async def test(self, ctx, arg):
-        await ctx.send("pong")
-
-
 
 def setup(client):
-    client.add_cog(define(client))
+    client.add_cog(dictionary(client))
