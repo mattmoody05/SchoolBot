e22dc43189e752f52067a348ec80b26f59b1c926
prathmeshvishwakarma <prathmeshvishwakarma13@gmail.com>
2020-10-11 15:23:35 +0530

    added summary command and uncomplete full page command
    the full page command need some paginator work rn

--- cogs/wikipedia.py
+++ cogs/wikipedia.py
@@ -1,7 +1,7 @@
 # imports
 import discord
-from discord.ext import commands
 import wikipediaapi
+from discord.ext import commands
 
 
 class Wikipedia(commands.Cog):
@@ -14,15 +14,42 @@ class Wikipedia(commands.Cog):
 
     @wiki.command()
     async def summary(self, ctx, *, query: str):
-        ww = wikipediaapi.Wikipedia('en')
-
+        ww = wikipediaapi.Wikipedia(language="en")
         page = ww.page(query)
 
-        title = page.title
-        summary = page.summary
-        print(title)
-        total = summary.split(".")
-        await ctx.send(". ".join(total[:5]))
+        if page.exists():
+            title = page.title
+            summary = page.summary.split(".")
+
+            read_more = f"\n\nYou can read more about this here : {page.fullurl}"
+
+            embed = discord.Embed()
+            embed.title = title
+            embed.description = ". ".join(summary[:10])
+            embed.description += read_more
+            embed.set_footer(text=f"Requested by - {ctx.author}")
+
+            await ctx.send(embed=embed)
+
+        else:
+            await ctx.send(f"{ctx.author.mention} please specify a valid Wikipedia Page :)")
+
+    @wiki.command()
+    async def full(self, ctx, *, query: str):
+        ww = wikipediaapi.Wikipedia(language="en", extract_format=wikipediaapi.ExtractFormat.WIKI)
+        page = ww.page(query)
+
+        if page.exists():
+            title = page.title
+            content = page.text
+
+            pager = commands.Paginator()
+            pager.add_line(line=f"Title : {title}")
+            pager.add_line(line=f"Full Text - ")
+
+        else:
+            await ctx.send(f"{ctx.author.mention} please specify a valid Wikipedia Page :)")
+
 
 def setup(client):
     client.add_cog(Wikipedia(client))
