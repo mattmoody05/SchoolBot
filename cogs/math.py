d362cbfbc713c23b403d84ac6c0c6a6bc2b71b56
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2020-10-18 14:55:48 +0100

    added embeds to math.py

--- cogs/math.py
+++ cogs/math.py
@@ -1,6 +1,19 @@
+# function to get an embed
+def SimpleEmbed(author):
+    Embed = discord.Embed(
+        colour = discord.Colour.light_gray()
+    )
+    Embed.set_author(name = "Answer: " + str(author))
+    return Embed
+
+# discord imports
 from discord.ext import commands
 import discord
+
+# other imports 
 import math
+
+
 
 
 class Math(commands.Cog):
@@ -18,90 +31,90 @@ class Math(commands.Cog):
     @math.command(aliases=["addition", "sum"])
     async def add(self, ctx, *args):
         add = sum(int(i) for i in args)
-        await ctx.send(add)
+        await ctx.send(embed = SimpleEmbed(add))
 
     @math.command()
     async def multiply(self, ctx, *args):
         mul = 1
         for i in map(int, args):
             mul *= i
-        await ctx.send(mul)
+        await ctx.send(embed = SimpleEmbed(mul))
 
     @math.command(aliases=["div"])
     async def divide(self, ctx, a: float, b: float):
         divide = a / b
-        await ctx.send(divide)
+        await ctx.send(embed = SimpleEmbed(divide))
 
     @math.command(aliases=["pow"])
     async def power(self, ctx, a: float, b: float):
         pow = a ** b
-        await ctx.send(pow)
+        await ctx.send(embed = SimpleEmbed(pow))
 
     @math.command(aliases=["root"])
     async def sqrt(self, ctx, a: float):
         sqrt = math.sqrt(a)
-        await ctx.send(sqrt)
+        await ctx.send(embed = SimpleEmbed(sqrt))
 
     @math.command()
     async def sin(self, ctx, a: float):
         sin = math.sin(a)
-        await ctx.send(sin)
+        await ctx.send(embed = SimpleEmbed(sin))
 
     @math.command()
     async def cos(self, ctx, a: float):
         cos = math.cos(a)
-        await ctx.send(cos)
+        await ctx.send(embed = SimpleEmbed(cos))
 
     @math.command()
     async def tan(self, ctx, a: float):
         tan = math.tan(a)
-        await ctx.send(tan)
+        await ctx.send(embed = SimpleEmbed(tan))
 
     @math.command(aliases=["cosecant", "asin"])
     async def cossec(self, ctx, a: float):
         cossec = math.asin(a)
-        await ctx.send(cossec)
+        await ctx.send(embed = SimpleEmbed(cossec))
 
     @math.command()
     async def sec(self, ctx, a: float):
         sec = math.acos(a)
-        await ctx.send(sec)
+        await ctx.send(embed = SimpleEmbed(sec))
 
     @math.command()
     async def cot(self, ctx, a: float):
         cot = math.atan(a)
-        await ctx.send(cot)
+        await ctx.send(embed = SimpleEmbed(cot))
 
     @math.command(aliases=["gcd"])
     async def hcf(self, ctx, a: int, b: int):
         gcd = math.gcd(a, b)
-        await ctx.send(gcd)
+        await ctx.send(embed = SimpleEmbed(gcd))
 
     @math.command()
     async def lcm(self, ctx, a: int, b: int):
         lcm = a * b // math.gcd(a, b)
-        await ctx.send(lcm)
+        await ctx.send(embed = SimpleEmbed(lcm))
 
     @math.command()
     async def factorial(self, ctx, a: int):
         fact = math.factorial(a)
-        await ctx.send(fact)
+        await ctx.send(embed = SimpleEmbed(fact))
 
     @math.command()
     async def gamma(self, ctx, a: float):
         gamma = math.gamma(a)
-        await ctx.send(gamma)
+        await ctx.send(embed = SimpleEmbed(gamma))
 
     @math.command(aliases=["floor"])
     async def round(self, ctx, a: float):
         floor = round(a)
-        await ctx.send(floor)
+        await ctx.send(embed = SimpleEmbed(floor))
 
     @math.command(aliases=["mean"])
     async def average(self, ctx, *numbers):
         add = sum(i for i in map(int, numbers))
         total = len(numbers)
-        await ctx.send(add/total)
+        await ctx.send(embed = SimpleEmbed(add/total))
 
 
 def setup(client):
