88205a3bd0eeaae71405d0e14aeda83f34592e01
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2020-10-24 11:41:11 +0100

    more img stuff
    - added img.py so that 'import img' can be used easily
    - started using images in embeds

--- cogs/help.py
+++ cogs/help.py
@@ -2,11 +2,14 @@
 from discord.ext import commands
 import discord
 
+# image urls
+import img
+
 # main embed
 MainEmbed = discord.Embed(
     colour=discord.Colour.light_gray()
 )
-MainEmbed.set_author(name="Schoolbot Help")
+MainEmbed.set_author(name="Schoolbot Help", icon_url = img.ImgMain)
 MainEmbed.add_field(name="Help Areas",
                     value="Below are the diffent areas of help you can access\nPlease run ***help <area>*** to get specific help. e.g ***help math***",
                     inline=False)
@@ -16,7 +19,7 @@ MainEmbed.add_field(name="Areas of help", value="- math\n- wikipedia\n- dictiona
 OtherEmbed = discord.Embed(
     colour=discord.Colour.light_gray()
 )
-OtherEmbed.set_author(name="Schoolbot - Other help")
+OtherEmbed.set_author(name="Schoolbot - Other help", icon_url = img.ImgMain)
 OtherEmbed.add_field(name="ping", value="Returns your latency to the bot in ms", inline=False)
 OtherEmbed.add_field(name="timer <h> <m> <s>", value="Sets a timer for the amount of time you have provided", inline=False)
 OtherEmbed.add_field(name="suggest", value="Makes a yes no poll", inline=False)
@@ -29,7 +32,7 @@ OtherEmbed.add_field(name='password_generate <len of password>', value="Gives a 
 WikiEmbed = discord.Embed(
     colour=discord.Colour.light_gray()
 )
-WikiEmbed.set_author(name="Schoolbot - Wikipedia help")
+WikiEmbed.set_author(name="Schoolbot - Wikipedia help", icon_url = img.ImgWikipedia)
 WikiEmbed.add_field(name="wiki summary <query>",
                     value="Searches wikipedia for your query, returning a summary of that topic", inline=False)
 WikiEmbed.add_field(name="wiki full <query>", value="Searches wikipedia for your query and returns the full article", inline=False)
@@ -38,7 +41,7 @@ WikiEmbed.add_field(name="wiki full <query>", value="Searches wikipedia for your
 DictEmbed = discord.Embed(
     colour=discord.Colour.light_gray()
 )
-DictEmbed.set_author(name="Schoolbot - Dictionary help")
+DictEmbed.set_author(name="Schoolbot - Dictionary help", icon_url = img.ImgDictionary)
 DictEmbed.add_field(name="dict define <word>", value="Defines the word provided", inline=False)
 DictEmbed.add_field(name="dict synonym <word>", value="Gives alternative words for the word provided", inline=False)
 
@@ -47,7 +50,7 @@ ModEmbed = discord.Embed(
     colour=discord.Colour.light_gray(),
     description="Please note that these moderation commands will require the correct permissions"
 )
-ModEmbed.set_author(name="Schoolbot - Moderation help")
+ModEmbed.set_author(name="Schoolbot - Moderation help", icon_url = img.ImgMain)
 ModEmbed.add_field(name="kick @member", value="Kicks the mentioned member", inline=False)
 ModEmbed.add_field(name="ban @member", value="Bans the mentioned member", inline=False)
 ModEmbed.add_field(name="purge <number>", value="Deletes the specified number of messages", inline=False)
@@ -57,7 +60,7 @@ MathEmbed = discord.Embed(
     colour=discord.Colour.light_gray(),
     description="The names of these commands are self-explanitary, please use common sense to determine what they do."
 )
-MathEmbed.set_author(name="Schoolbot - Math help")
+MathEmbed.set_author(name="Schoolbot - Math help", icon_url = img.ImgMath)
 MathEmbed.add_field(name="Commands",
                     value="math add\nmath multiply\nmath divide\nmath power\nmath root\nmath sin\nmath cos\nmath tan\nmath cossec\nmath sec\nmath cot\nmath hcf\nmath lcm\nmath factorial\nmath gamma\nmath round\nmath average")
 
@@ -65,13 +68,13 @@ MathEmbed.add_field(name="Commands",
 YTEmbed = discord.Embed(
     colour=discord.Colour.light_gray()
 )
-YTEmbed.set_author(name="Schoolbot help - Youtube")
+YTEmbed.set_author(name="Schoolbot help - Youtube", icon_url=img.ImgYoutube)
 YTEmbed.add_field(name="youtube search <query>", value="Searches youtube and returns info on your query", inline=False)
 
 # time-table embed
 TTEmbed = discord.Embed(
     colour=discord.Colour.light_grey())
-TTEmbed.set_author(name="Schoolbot help - Time Table")
+TTEmbed.set_author(name="Schoolbot help - Time Table", icon_url=img.ImgTimetable)
 TTEmbed.add_field(name="time_table insert <Name of the day> <Time in GMT> <Work>", value="Reminds you for for that task in your DM's \n e.g - $time_table insert Monday 03:33 Maths class", inline=False)
 TTEmbed.add_field(name="time_table delete <Name of the day> <Time>", value="Deletes the task if it exists", inline=False)
 TTEmbed.add_field(name="time_table all", value="Sends all your schedule", inline=False)
@@ -79,19 +82,19 @@ TTEmbed.add_field(name="time_table all", value="Sends all your schedule", inline
 # todo
 TDEmbed = discord.Embed(
     colour=discord.Colour.light_gray())
-TDEmbed.set_author(name="Schoolbot help - Todo")
+TDEmbed.set_author(name="Schoolbot help - Todo", icon_url=img.ImgTodo)
 TDEmbed.add_field(name="todo insert <work>", value="Inserts the work in your todo list", inline=False)
 TDEmbed.add_field(name="todo delete <work>", value="Deletes the work from your todo list", inline=False)
 TDEmbed.add_field(name="todo all", value="Sends your todo list")
 
 # notes sharing
 NSEmbed = discord.Embed(colour=discord.Colour.light_grey())
-NSEmbed.set_author(name="Schoolbot help - Notes Sharing")
+NSEmbed.set_author(name="Schoolbot help - Notes Sharing", icon_url=img.ImgNotes)
 NSEmbed.add_field(name="Pasting with - <title> | <description>", value="Just paste the picture and it would be formatted into a well embed \n Note: The picture should be in a proper channel name e.g - #notes-sharing")
 
 # tag
 TEmbed = discord.Embed(color=discord.Colour.light_grey())
-TEmbed.set_author(name="Schoolbot help - Tag")
+TEmbed.set_author(name="Schoolbot help - Tag", icon_url=img.ImgTag)
 TEmbed.add_field(name="tag create <name> <content>", value="Creates a tag with that name and content", inline=False)
 TEmbed.add_field(name="tag edit <name> <content>", value="Edits the tag with that name if you own that", inline=False)
 TEmbed.add_field(name="tag rename <old name> <new name>", value="Renames the tag with that name if you own it", inline=False)
@@ -102,7 +105,7 @@ TEmbed.add_field(name="tag all", value="DM's all the tags", inline=False)
 
 # translate
 TSEmbed = discord.Embed(color=discord.Colour.light_grey())
-TSEmbed.set_author(name="Schoolbot help - Translator")
+TSEmbed.set_author(name="Schoolbot help - Translator", icon_url=img.ImgTranslate)
 TSEmbed.add_field(name="translate <language unicode> <text>", value="Translate the text in the language provided by you", inline=False)
 TSEmbed.add_field(name="translate detect <text>", value="Detects the language of the text that you have entered", inline=False)
 TSEmbed.add_field(name="translate all", value="Gives all the unicode of all the languages supported", inline=False)
@@ -111,7 +114,7 @@ TSEmbed.add_field(name="translate all", value="Gives all the unicode of all the 
 CurrencyEmbed = discord.Embed(
     colour = discord.Colour.light_gray()
 )
-CurrencyEmbed.set_author(name = "Schoolbot help - Currency")
+CurrencyEmbed.set_author(name = "Schoolbot help - Currency", icon_url=img.ImgCurrency)
 CurrencyEmbed.add_field(name = "currency convert <amount> <input currency> <output currency>", value = "Converts the amount specified from the input currency specified to the output currency specified", inline = False)
 CurrencyEmbed.add_field(name = "currency value <currency>", value = "Show the value of the input currency in USD", inline = False)
 CurrencyEmbed.add_field(name = "currency list", value = "Shows a list of all the currencies supported by the converter", inline = False)
