a8b61d7c158141eb6948cf72287ed5574281dacc
prathmeshvishwakarma <prathmeshvishwakarma13@gmail.com>
2020-10-26 11:35:48 +0530

    postgre file updated

--- other/postgreSQL-install.py
+++ other/postgreSQL-install.py
@@ -6,11 +6,39 @@ import asyncio
 
 async def main():
     conn = await asyncpg.connect(host = "localhost", user = "postgres", database = "postgres", port = PORT, password = PASSWORD)
-    query = """CREATE TABLE study_mode(
+
+    # study_mode
+    study_mode_query = """CREATE TABLE study_mode(
         user_id BIGINT NOT NULL,
         mode BOOL NOT NULL
     )"""
-    await conn.execute(query)
+    await conn.execute(study_mode_query)
+
+    # tag
+    tag_query = """CREATE TABLE tag(
+        user_id BIGINT NOT NULL,
+        name TEXT NOT NULL,
+        text TEXT NOT NULL,
+        uses INT NOT NULL
+    )"""
+    await conn.execute(tag_query)
+
+    # todo
+    todo_query = """CREATE TABLE todo(
+        user_id BIGINT NOT NULL,
+        work TEXT NOT NULL,
+        time TEXT NOT NULL
+    )"""
+    await conn.execute(todo_query)
+
+    # time_table
+    time_table_query = """CREATE TABLE time_table(
+        day TEXT NOT NULL,
+        user_id BIGINT NOT NULL,
+        time TEXT NOT NULL,
+        work TEXT NOT NULL
+    )"""
+    await conn.execute(time_table_query)
 
 if __name__ == '__main__':
     task = asyncio.get_event_loop()
